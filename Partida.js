23fd59d10ea1753ac31a26334e332a193f333958
unknown <you@example.com>
2020-12-09 19:27:30 +0100

    OL - Añadir Tabla a partida.js

---
+++ Partida.js
@@ -0,0 +1,45 @@
+let Partida = {
+
+    generarTabla: function(coordenadas){
+
+        let body = document.getElementsByTagName("body")[0];
+
+        let table = document.createElement("table");
+        let tbody = document.createElement("tbody");
+
+        for (let i = 0; i < coordenadas; i++){
+
+            let row = document.createElement("tr");
+
+            for (let j = 0; j < coordenadas; j++){
+
+                let cell = document.createElement("td");
+
+                row.appendChild(cell);
+
+            }
+
+
+            tbody.appendChild(row);
+        }
+
+        table.appendChild(tbody);
+        body.appendChild(table);
+
+        table.setAttribute('border', 2);
+        table.setAttribute('id', 'table');
+
+    },
+    
+//Objecte pare elements
+     Elements: function(nom){
+        var nom = nom;
+        this.getNom = function(){
+            return nom;
+        }
+        this.setNom = function(_nom){
+            nom = _nom;
+        }
+    }
+
+}
--- main.js
+++ main.js
@@ -14,47 +14,8 @@ window.onload = function(){
 
     }
 
-    function generarTabla(){
-
-        let body = document.getElementsByTagName("body")[0];
-
-        let table = document.createElement("table");
-        let tbody = document.createElement("tbody");
-
-        for (let i = 0; i < coordenadas; i++){
-
-            let row = document.createElement("tr");
-
-            for (let j = 0; j < coordenadas; j++){
-
-                let cell = document.createElement("td");
-
-                row.appendChild(cell);
-
-            }
-
-
-            tbody.appendChild(row);
-        }
-
-        table.appendChild(tbody);
-        body.appendChild(table);
-
-        table.setAttribute('border', 2);
-        table.setAttribute('id', 'table');
-
-    }
-    generarTabla();
-
+    Partida.generarTabla(coordenadas);
 }
 
-//Objecte pare elements
-var Elements = function(nom){
-    var nom = nom;
-    this.getNom = function(){
-        return nom;
-    }
-    this.setNom = function(_nom){
-        nom = _nom;
-    }
-}
+
+
