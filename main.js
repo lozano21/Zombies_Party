59904c5baf8f2cfc91715d25d17146eb32307cea
gerardmm78 <gerardmnm@gmail.com>
2020-12-14 20:40:57 +0100

    gmm - canvi textarea  a input

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.onload = function(){
     var coordenadas = 0;
     coordenadas = parseInt(prompt("Medida del tablero "));
 
-    while(coordenadas === "" || coordenadas == null || coordenadas < 5 || coordenadas > 20){//bucle do while para el tamaño de la tabla
+    while(coordenadas === "" || coordenadas == null || coordenadas < 5 || coordenadas > 20){//bucle while para el tamaño de la tabla
 
         alert("Por favor introduce la medida, entre 5 y 20");
         coordenadas = parseInt(prompt("Medida del tablero (5-20)"));
